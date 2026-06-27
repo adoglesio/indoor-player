ec755f0df4a834b04446d3d5afdd366b9bab16d4
Adoglésio Gomes <57054219+adoglesio@users.noreply.github.com>
2026-06-27 19:10:14 -0300

    Remove fetch and activate event listeners from service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,39 +1,3 @@
 // src/service-worker.ts
 /* eslint-disable no-restricted-globals */
 /// <reference lib="webworker" />
-
-const sw = self as unknown as ServiceWorkerGlobalScope;
-
-const CACHE_NAME = 'indoor-media-v1';
-
-sw.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  
-  if (url.hostname.includes('s3.amazonaws.com') || url.hostname.includes('cloudfront.net')) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(async (cache) => {
-        const cachedResponse = await cache.match(event.request);
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        const response = await fetch(event.request);
-        cache.put(event.request, response.clone());
-        return response;
-      })
-    );
-  }
-});
-
-sw.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
